5713c17d6a783a50b5157db7e15dd04a4a808bc0
david m flynn <flynndave1@msn.com>
2018-07-28 04:07:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // write your code below!
+//Happy Coding Day
+var hh = "Happy holidays"
+const special = ("Easter" , "New Year" , "Labor Day") 
+var md  = "Mother\'s Day";
+
+function happyHolidays ()  {
+  return ("Happy holidays!");
+}
+
+function happyHolidaysTo (namen) {
+  console.log("Second Function!")
+  return( "Happy holidays to " + namen +"!2" ) ;
+}
+
+function happyHolidaysTo (theDay , namen) {
+  return  ("Happy " + theDay + " \, "  + namen + "!3" ) 
+}
+
+function holidayCountdown (theDay ,  daysTill) {
+  return  ("It\'s " +  daysTill + " days until " + theDay +"!4" ) 
+}
+
+
+/*for (i = 0; i < special.length; i++) { */
+  console.log( happyHolidays()) ;
+  console.log(happyHolidaysTo( "James"));
+  console.log(happyHolidaysTo("James" , special[2]));
+  console.log(holidayCountdown(md , 25));
+  
+  console.log( happyHolidays()) ;
+  console.log( happyHolidaysTo( "James"));
+  console.log( happyHolidaysTo(md ,"James"));
+  console.log( holidayCountdown(md , 25));
+  
+   happyHolidays() ;
+   happyHolidaysTo( "James");
+  happyHolidaysTo(md ,"James");
+  holidayCountdown(md , 25);
+  
